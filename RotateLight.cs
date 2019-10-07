ad1fdf6861d5d383b2c8c9874a9ff1d41749cf01
LynnetteTelfer <56223008+LynnetteTelfer@users.noreply.github.com>
2019-10-07 15:00:59 +1100

    Add files via upload

---
+++ RotateLight.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateLight : MonoBehaviour {
+
+	public Vector3 rotation = Vector3.zero;
+
+	public float offset = -20f;
+	public float max = 220f;
+	public float speed = 20f;
+
+	// Update is called once per frame
+	void Update () {
+
+		rotation.y = -(offset + Mathf.Max( Time.time * speed, max ));
+		transform.localEulerAngles = rotation;
+
+	}
+}
