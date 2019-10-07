b5adb6a662068511a9b6c657aacdd6bcdda50268
LynnetteTelfer <56223008+LynnetteTelfer@users.noreply.github.com>
2019-10-07 15:03:10 +1100

    Add files via upload

---
+++ RotateCubes.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCubes : MonoBehaviour {
+
+
+	public Vector3 rotation = Vector3.zero;
+	public float speed = 45f;
+
+	// Update is called once per frame
+	void Update () {
+
+		rotation.y += Time.deltaTime * speed;
+		transform.localEulerAngles = rotation;
+
+	}
+}
